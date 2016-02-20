c2e38f763abc812c306433d0423e4c19fbb4dc8f
mdlee12 <mdlee12@gmail.com>
2016-02-20 13:53:23 -0500

    updated library information

--- MLB_Scraper_Batting.R
+++ MLB_Scraper_Batting.R
@@ -1,134 +1,146 @@
-c('rvest','dplyr','pipeR') -> packages #installs packages
-lapply(packages, library, character.only = T) #installs packages
+# install.packages("devtools")
+# install_github("hadley/rvest")
+c('rvest','dplyr') -> packages #installs packages
+lapply(packages, library, character.only = T)
+
+
 url <- "http://www.baseball-reference.com/teams/"
 teams=c("ARI","ATL","BAL","BOS","CHC","CHW","CIN","CLE","COL","DET","HOU","KCR","LAD","MIA","MIL","MIN","NYM","NYY","OAK","PHI","PIT","SDP","SFG","SEA","STL","TEX","TOR","TBR")
-'#team_batting' -> stats_table
-stats_table %>>% paste0(stats_table,' a') -> stats_id
+stats_table <- '#team_batting'
+stats_id <- paste0(stats_table,' a')
 majors_batting <- data.frame()
 
-for (teams in teams){ for (season in 2014:2000) {
-html <- paste(url,teams,"/",season,".shtml",sep="")
+for (teams in teams){ 
+	for (season in 2015:2000) {
+		html <- paste(url,teams,"/",season,".shtml",sep="")
 
-	html(html) %>%
-		html_nodes(stats_table) %>%
-		html_table(header = T) %>%
-		data.frame() %>%
-		tbl_df() -> df
+		read_html(html) %>%
+			html_nodes(stats_table) %>%
+			html_table(header = T) %>%
+			data.frame() %>%
+			tbl_df() -> df
 
-	html(html) %>>%
-		html_nodes(stats_id) %>>%
-		html_attr(name="href") %>>% unlist %>>% as.character -> maj_playerid
-	df %>>% nrow()-5  -> rows
-	    df[1:rows,] -> df
-	df=df[!na.omit(df$Rk=='Rk'),]
-	df$season <- c(season)
-	df$Tm <- c(teams)
-	df$Lev=c("MLB")
-	maj_playerid <- gsub(".shtml","",maj_playerid)
-	maj_playerid=substring(maj_playerid, 12)
-	df$maj_playerid <- c(maj_playerid)
+		read_html(html) %>%
+			html_nodes(stats_id) %>%
+			html_attr(name="href") %>% unlist %>% as.character -> maj_playerid
 
+		rows <- nrow(df)-5
+		df <- df[1:rows,]
+		df <- df[!na.omit(df$Rk=='Rk'),]
+		df$season <- season
+		df$teams <- teams
+		df$level <- "MLB"
 
-majors_batting <- rbind(majors_batting,df)
-
-}
+		maj_playerid <- gsub(".shtml","",maj_playerid)
+		maj_playerid=substring(maj_playerid, 12)
+		df$maj_playerid <- c(maj_playerid)
+		
+		majors_batting <- rbind(majors_batting,df)
+	}
 }
 
 teams=c("TBD")
-'#team_batting' -> stats_table
-stats_table %>>% paste0(stats_table,' a') -> stats_id
+stats_table <- '#team_batting'
+stats_id <- paste0(stats_table,' a')
 majors_batting_tam <- data.frame()
 
-for (teams in teams){ for (season in 2007:2000) {
-    html <- paste(url,teams,"/",season,".shtml",sep="")
-    
-	html(html) %>%
-		html_nodes(stats_table) %>%
-		html_table(header = T) %>%
-		data.frame() %>%
-		tbl_df() -> df
+for (teams in teams){ 
+	for (season in 2007:2000) {
+		html <- paste(url,teams,"/",season,".shtml",sep="")
 
-	html(html) %>>%
-		html_nodes(stats_id) %>>%
-		html_attr(name="href") %>>% unlist %>>% as.character -> maj_playerid
-	df %>>% nrow()-5  -> rows
-	    df[1:rows,] -> df
-	df=df[!na.omit(df$Rk=='Rk'),]
-	df$season <- c(season)
-	df$Tm <- c(teams)
-	df$Lev=c("MLB")
-	maj_playerid <- gsub(".shtml","",maj_playerid)
-	maj_playerid=substring(maj_playerid, 12)
-	df$maj_playerid <- c(maj_playerid)
-    
-    majors_batting_tam <- rbind(majors_batting_tam,df)
-    
-}
+		read_html(html) %>%
+			html_nodes(stats_table) %>%
+			html_table(header = T) %>%
+			data.frame() %>%
+			tbl_df() -> df
+
+		read_html(html) %>%
+			html_nodes(stats_id) %>%
+			html_attr(name="href") %>% unlist %>% as.character -> maj_playerid
+
+		rows <- nrow(df)-5
+		df <- df[1:rows,]
+		df <- df[!na.omit(df$Rk=='Rk'),]
+		df$season <- season
+		df$teams <- teams
+		df$level <- "MLB"
+
+		maj_playerid <- gsub(".shtml","",maj_playerid)
+		maj_playerid=substring(maj_playerid, 12)
+		df$maj_playerid <- c(maj_playerid)
+		
+		majors_batting_tam <- rbind(majors_batting_tam,df)
+	}
 }
 
 
 teams=c("WSN","LAA")
-'#team_batting' -> stats_table
-stats_table %>>% paste0(stats_table,' a') -> stats_id
+stats_table <- '#team_batting'
+stats_id <- paste0(stats_table,' a')
 majors_batting_new <- data.frame()
 
-for (teams in teams){ for (season in 2014:2005) {
-    html <- paste(url,teams,"/",season,".shtml",sep="")
-    
-	html(html) %>%
-		html_nodes(stats_table) %>%
-		html_table(header = T) %>%
-		data.frame() %>%
-		tbl_df() -> df
+for (teams in teams){ 
+	for (season in 2014:2005) {
+		html <- paste(url,teams,"/",season,".shtml",sep="")
 
-	html(html) %>>%
-		html_nodes(stats_id) %>>%
-		html_attr(name="href") %>>% unlist %>>% as.character -> maj_playerid
-	df %>>% nrow()-5  -> rows
-	    df[1:rows,] -> df
-	df=df[!na.omit(df$Rk=='Rk'),]
-	df$season <- c(season)
-	df$Tm <- c(teams)
-	df$Lev=c("MLB")
-	maj_playerid <- gsub(".shtml","",maj_playerid)
-	maj_playerid=substring(maj_playerid, 12)
-	df$maj_playerid <- c(maj_playerid)
-    
-    majors_batting_new <- rbind(majors_batting_new,df)
-    
-}
+		read_html(html) %>%
+			html_nodes(stats_table) %>%
+			html_table(header = T) %>%
+			data.frame() %>%
+			tbl_df() -> df
+
+		read_html(html) %>%
+			html_nodes(stats_id) %>%
+			html_attr(name="href") %>% unlist %>% as.character -> maj_playerid
+
+		rows <- nrow(df)-5
+		df <- df[1:rows,]
+		df <- df[!na.omit(df$Rk=='Rk'),]
+		df$season <- season
+		df$teams <- teams
+		df$level <- "MLB"
+
+		maj_playerid <- gsub(".shtml","",maj_playerid)
+		maj_playerid=substring(maj_playerid, 12)
+		df$maj_playerid <- c(maj_playerid)
+		
+		majors_batting_new <- rbind(majors_batting_new,df)
+	}
 }
 
 teams=c("MON","ANA")
-'#team_batting' -> stats_table
-stats_table %>>% paste0(stats_table,' a') -> stats_id
+stats_table <- '#team_batting'
+stats_id <- paste0(stats_table,' a')
 majors_batting_extra <- data.frame()
 
-for (teams in teams){ for (season in 2000:2004) {
-    html <- paste(url,teams,"/",season,".shtml",sep="")
-    
-	html(html) %>%
-		html_nodes(stats_table) %>%
-		html_table(header = T) %>%
-		data.frame() %>%
-		tbl_df() -> df
+for (teams in teams){ 
+	for (season in 2004:2000) {
+		html <- paste(url,teams,"/",season,".shtml",sep="")
 
-	html(html) %>>%
-		html_nodes(stats_id) %>>%
-		html_attr(name="href") %>>% unlist %>>% as.character -> maj_playerid
-	df %>>% nrow()-5  -> rows
-	    df[1:rows,] -> df
-	df=df[!na.omit(df$Rk=='Rk'),]
-	df$season <- c(season)
-	df$Tm <- c(teams)
-	df$Lev=c("MLB")
-	maj_playerid <- gsub(".shtml","",maj_playerid)
-	maj_playerid=substring(maj_playerid, 12)
-	df$maj_playerid <- c(maj_playerid)
-    
-    majors_batting_extra <- rbind(majors_batting_extra,df)
-    
-}
+		read_html(html) %>%
+			html_nodes(stats_table) %>%
+			html_table(header = T) %>%
+			data.frame() %>%
+			tbl_df() -> df
+
+		read_html(html) %>%
+			html_nodes(stats_id) %>%
+			html_attr(name="href") %>% unlist %>% as.character -> maj_playerid
+
+		rows <- nrow(df)-5
+		df <- df[1:rows,]
+		df <- df[!na.omit(df$Rk=='Rk'),]
+		df$season <- season
+		df$teams <- teams
+		df$level <- "MLB"
+
+		maj_playerid <- gsub(".shtml","",maj_playerid)
+		maj_playerid=substring(maj_playerid, 12)
+		df$maj_playerid <- c(maj_playerid)
+		
+		majors_batting_extra <- rbind(majors_batting_extra,df)
+	}
 }
 
 majors_batting=rbind(majors_batting,majors_batting_tam,majors_batting_new,majors_batting_extra)
+rm(majors_batting_tam,majors_batting_new,majors_batting_extra)
--- MiLB_Scraper_Pitching.R
+++ MiLB_Scraper_Pitching.R
@@ -1,33 +1,36 @@
 # install.packages("devtools")
-# install_github("hadley/rvest"
-c('rvest','dplyr','pipeR') -> packages #installs packages
-lapply(packages, library, character.only = T) #installs packages
+# install_github("hadley/rvest")
+c('rvest','dplyr') -> packages #installs packages
+lapply(packages, library, character.only = T)
+
 url <- "http://www.baseball-reference.com/minors/"
 teams=c("ARI","ATL","BAL","BOS","CHC","CHW","CIN","CLE","COL","DET","HOU","KCR","ANA","LAD","FLA","MIL","MIN","NYM","NYY","OAK","PHI","PIT","SDP","SFG","SEA","STL","TBD","TEX","TOR","WSN")
-'#team_pitching.sortable.stats_table' -> stats_table
-stats_table %>>% paste0(stats_table,' a') -> stats_id
-min_pitching <- data.frame()
+stats_table <- '#team_pitching.sortable.stats_table'
+stats_id <- paste0(stats_table,' a')
+minors_pitching <- data.frame()
 
-for (teams in teams){ for (season in 2014:2014) {
-html <- paste(url,"affiliate.cgi?id=",teams,"&year=",season,sep="")
+for (teams in teams){ 
+	for (season in 2015:2000) {
+		html <- paste(url,"affiliate.cgi?id=",teams,"&year=",season,sep="")
 
-html(html) %>%
-	html_nodes(stats_table) %>%
-	html_table(header = T) %>%
-	data.frame() %>%
-	tbl_df() -> df
+		read_html(html) %>%
+			html_nodes(stats_table) %>%
+			html_table(header = T) %>%
+			data.frame() %>%
+			tbl_df() -> df
 
-html(html) %>>%
-        html_nodes(stats_id) %>>%
-        html_attr(name="href") %>>% unlist %>>% as.character -> min_playerid
-df %>>% nrow() -> rows
-    df[1:rows,] -> df
-df=df[!na.omit(df$Rk=='Rk'),]
-df$season <- c(season)
-min_playerid=substr(min_playerid, 23,34)
-df$min_playerid <- c(min_playerid)
+		read_html(html) %>%
+			html_nodes(stats_id) %>%
+			html_attr(name="href") %>% unlist %>% as.character -> min_playerid
 
-min_pitching <- rbind(min_pitching,df)
+		df <- df[1:nrow(df),]
+		df <- df[!na.omit(df$Rk=='Rk'),]
+		df$season <- c(season)
+		df$teams <- c(teams)
 
-}
+		min_playerid=gsub("/register/player.cgi?id=", "", min_playerid,fixed = TRUE)
+		df$min_playerid <- c(min_playerid)
+
+		minors_pitching <- rbind(minors_pitching,df)
+	}
 }
